268c9a93893ce76e9ec635d6532332e90b67e2ad
Giada Carminati <=>
2014-10-04 18:13:40 -0700

    After refreshing db

--- app/whodoesmyhair.py
+++ app/whodoesmyhair.py
@@ -35,108 +35,106 @@ def result():
     salon = request.form.get("salonname", None)
     title = salon
 
-    return render_template("stylist2.html",salon=salon)
-
     # query 1:
     # from salon_name get salon_id
-#    query_id="SELECT business_id, has_stylists FROM VegasSalonNames WHERE name = '%s'" % salon
-#    cnx.cur.execute(query_id)
-#    raw = cnx.cur.fetchone()
-##    if not raw:
-##        cnx.cur.close()
-##        return render_template("error.html")
-#
-#    salon_id = raw[0]
-#    has_stylist = raw[1]
-#
-#    # query 2:
-#    # from salon_id get address, salon_rating, salon_number_of_reviews,
-#    # stylist_names and salon_number_of_reviews_with_stylist_name
-#    query = "SELECT full_address, stars, review_count, stylists, review_stylists FROM salonsVegas WHERE business_id = '%s'" % salon_id 
-#    cnx.cur.execute(query)
-#    raw2 = cnx.cur.fetchall()
-#    cnx.cur.close()
-#
-#    # 2.1: format the address info
-#    address = [ row[0] for row in raw2 ]
-#    a = str(address[0])
-#    ad = a.split(",")
-#    add = sorted(ad)
-#    location = ""
-#    street = ""
-#    city = ""
-#    for n in add:
-#        if n.startswith(" NV"): city += "Las Vegas,"+n       
-#        elif n[0].isdigit(): street = n
-#        elif n.startswith("Ste"): street += ", "+n
-#        else: location = n
-#       
-#    # 2.2: convert salon_rating in images
-#    s = [row[1] for row in raw2]
-#    bsn_stars = float(s[0])
-#
-#    if   bsn_stars == 1:   imagesrc = "static/images/10stars.png"
-#    elif bsn_stars == 1.5: imagesrc = "static/images/15stars.png"
-#    elif bsn_stars == 2:   imagesrc = "static/images/20stars.png"
-#    elif bsn_stars == 2.5: imagesrc = "static/images/25stars.png"
-#    elif bsn_stars == 3:   imagesrc = "static/images/30stars.png"
-#    elif bsn_stars == 3.5: imagesrc = "static/images/35stars.png"
-#    elif bsn_stars == 4:   imagesrc = "static/images/40stars.png"
-#    elif bsn_stars == 4.5: imagesrc = "static/images/45stars.png"
-#    elif bsn_stars == 5:   imagesrc = "static/images/50stars.png"
-#    
-#    # 2.3: extract salon_number_of_reviews
-#    rc = [row[2] for row in raw2]
-#    review_count = int(rc[0])
-#
-#    # ERROR CONTROL:
-#    # if the business doesn't have a stylist, report an error page
-#    if not int(has_stylist):
-#        return render_template("stylist2.html",tab="stylist",
-#                               from_url="/stylist",salon=salon,
-#                               street=street,location=location,city=city,
-#                               bsn_stars=bsn_stars,
-#                               review_count=review_count,imagesrc=imagesrc)
-#    else:
-#        # 2.4: extract stylist_names
-#        names = [row[3] for row in raw2]
-#        n = names[0]
-#        stylists = n.split(" ")
-#       
-#        # 2.5: extract salon_number_of_reviews_with_stylist_name
-#        tr = [row[4] for row in raw2]
-#        totreviews = float(tr[0])
-#   
-#        # 3.1: calculate the highest rated stylists
-#        fdist = FreqDist(stylists)
-#
-#        # ERROR CONTROL:
-#        # we need at least 3 stylists
-#        if len(fdist) < 4:
-#            return render_template("stylist2.html",tab="stylist",
-#                                   from_url="/stylist",salon=salon,
-#                                   street=street,location=location,
-#                                   city=city,bsn_stars=bsn_stars,
-#                                   review_count=review_count,
-#                                   imagesrc=imagesrc)
-#        else:
-#            rating = {}
-#            for styl in range(0,len(fdist)):
-#                score = WilsonScoreInterval.WilsonScore(str(fdist.keys()[styl]),salon_id)
-#                rating[str(fdist.keys()[styl])] = score
-#                
-#                hey = sorted(rating.items(),key=operator.itemgetter(1))
-#                names = [thing[0] for thing in reversed(hey)]
-#                scores = [str(thing[1])[:4] for thing in reversed(hey)]
-#
-#    return render_template("stylist2.html",tab="stylist",
-#                           from_url="/stylist",salon=salon,
-#                           street=street,location=location,city=city,
-#                           bsn_stars=bsn_stars,review_count=review_count,
-#                           imagesrc=imagesrc,
-#                           name0=names[0],score0=scores[0],
-#                           name1=names[1],score1=scores[1],
-#                           name2=names[2],score2=scores[2])
+    query_id="SELECT business_id, has_stylists FROM VegasSalonNames WHERE name = '%s'" % salon
+    cnx.cur.execute(query_id)
+    raw = cnx.cur.fetchone()
+    if not raw:
+        cnx.cur.close()
+        return render_template("error.html")
+
+    salon_id = raw[0]
+    has_stylist = raw[1]
+
+    # query 2:
+    # from salon_id get address, salon_rating, salon_number_of_reviews,
+    # stylist_names and salon_number_of_reviews_with_stylist_name
+    query = "SELECT full_address, stars, review_count, stylists, review_stylists FROM salonsVegas WHERE business_id = '%s'" % salon_id 
+    cnx.cur.execute(query)
+    raw2 = cnx.cur.fetchall()
+    cnx.cur.close()
+
+    # 2.1: format the address info
+    address = [ row[0] for row in raw2 ]
+    a = str(address[0])
+    ad = a.split(",")
+    add = sorted(ad)
+    location = ""
+    street = ""
+    city = ""
+    for n in add:
+        if n.startswith(" NV"): city += "Las Vegas,"+n       
+        elif n[0].isdigit(): street = n
+        elif n.startswith("Ste"): street += ", "+n
+        else: location = n
+       
+    # 2.2: convert salon_rating in images
+    s = [row[1] for row in raw2]
+    bsn_stars = float(s[0])
+
+    if   bsn_stars == 1:   imagesrc = "static/images/10stars.png"
+    elif bsn_stars == 1.5: imagesrc = "static/images/15stars.png"
+    elif bsn_stars == 2:   imagesrc = "static/images/20stars.png"
+    elif bsn_stars == 2.5: imagesrc = "static/images/25stars.png"
+    elif bsn_stars == 3:   imagesrc = "static/images/30stars.png"
+    elif bsn_stars == 3.5: imagesrc = "static/images/35stars.png"
+    elif bsn_stars == 4:   imagesrc = "static/images/40stars.png"
+    elif bsn_stars == 4.5: imagesrc = "static/images/45stars.png"
+    elif bsn_stars == 5:   imagesrc = "static/images/50stars.png"
+    
+    # 2.3: extract salon_number_of_reviews
+    rc = [row[2] for row in raw2]
+    review_count = int(rc[0])
+
+    # ERROR CONTROL:
+    # if the business doesn't have a stylist, report an error page
+    if not int(has_stylist):
+        return render_template("stylist2.html",tab="stylist",
+                               from_url="/stylist",salon=salon,
+                               street=street,location=location,city=city,
+                               bsn_stars=bsn_stars,
+                               review_count=review_count,imagesrc=imagesrc)
+    else:
+        # 2.4: extract stylist_names
+        names = [row[3] for row in raw2]
+        n = names[0]
+        stylists = n.split(" ")
+       
+        # 2.5: extract salon_number_of_reviews_with_stylist_name
+        tr = [row[4] for row in raw2]
+        totreviews = float(tr[0])
+   
+        # 3.1: calculate the highest rated stylists
+        fdist = FreqDist(stylists)
+
+        # ERROR CONTROL:
+        # we need at least 3 stylists
+        if len(fdist) < 4:
+            return render_template("stylist2.html",tab="stylist",
+                                   from_url="/stylist",salon=salon,
+                                   street=street,location=location,
+                                   city=city,bsn_stars=bsn_stars,
+                                   review_count=review_count,
+                                   imagesrc=imagesrc)
+        else:
+            rating = {}
+            for styl in range(0,len(fdist)):
+                score = WilsonScoreInterval.WilsonScore(str(fdist.keys()[styl]),salon_id)
+                rating[str(fdist.keys()[styl])] = score
+                
+                hey = sorted(rating.items(),key=operator.itemgetter(1))
+                names = [thing[0] for thing in reversed(hey)]
+                scores = [str(thing[1])[:4] for thing in reversed(hey)]
+
+    return render_template("stylist2.html",tab="stylist",
+                           from_url="/stylist",salon=salon,
+                           street=street,location=location,city=city,
+                           bsn_stars=bsn_stars,review_count=review_count,
+                           imagesrc=imagesrc,
+                           name0=names[0],score0=scores[0],
+                           name1=names[1],score1=scores[1],
+                           name2=names[2],score2=scores[2])
 
 @app.route('/slides', methods=["GET"])
 def slides():
